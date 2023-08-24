0c7a3afae22d9d0ebe82226a65416d926802a0c0
luciopeloso <luciodemourapeloso@gmail.com>
2023-08-24 01:30:16 -0300

    termino scrollview, começando EntriesFragment

--- app/src/main/java/com/example/tcc/ui/EntriesFragment.kt
+++ app/src/main/java/com/example/tcc/ui/EntriesFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tcc.model.ParentData
@@ -30,6 +34,11 @@ class EntriesFragment : Fragment() {
     private val entryAccomplishedList = mutableListOf<ChildData>()
     private lateinit var parentList: MutableList<ParentData>
 
+    private lateinit var propertyList: MutableList<String>
+    private lateinit var areaList: MutableList<String>
+    private lateinit var vintageList: MutableList<String>
+
+
     private lateinit var entryAdapter: EntriesAdapter
 
     override fun onCreateView(
@@ -46,15 +55,53 @@ class EntriesFragment : Fragment() {
 
         auth = Firebase.auth
 
+        areaList = mutableListOf("Selecione um talhão")
+        propertyList = mutableListOf("Selecione uma propriedade")
+        vintageList = mutableListOf("Selecione uma safra")
+
+        getSpinners()
+
+        binding.spinnerPropiety.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                }
+            }
+        binding.spinnerArea.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
+        binding.spinnerYear.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
         parentList = ArrayList()
 
         val parentData: MutableList<String> =
-            mutableListOf("Produto",
+            mutableListOf(
+                "Produto",
                 "Sementes",
                 "Capina, dessecação e pós emergência",
                 "Fungicidas, Inseticidas e Foliares",
                 "Operações",
-                "Descritivo")
+                "Descritivo"
+            )
 
 //        val childDataData1: MutableList<ChildData> = mutableListOf(
 //            ChildData("Anathapur"),
@@ -76,7 +123,7 @@ class EntriesFragment : Fragment() {
 //        val parentObj4 = ParentData(parentTitle = parentData[1], subList = childDataData3)
 
 
-        for(i in 0 until parentData.size){
+        for (i in 0 until parentData.size) {
             val parentObj = ParentData(parentTitle = parentData[i])
             parentList.add(parentObj)
         }
@@ -99,10 +146,36 @@ class EntriesFragment : Fragment() {
 
     }
 
+    private fun getSpinners() {
+
+        db.collection("Property").whereArrayContains("users", auth.currentUser?.uid.toString())
+            .get().addOnSuccessListener { result ->
+                for (document in result) {
+                    val name = document.get("name").toString()
+                    propertyList.add(name)
+                }
+            }
+
+        initAdapterSpinner(binding.spinnerPropiety,propertyList)
+        initAdapterSpinner(binding.spinnerArea,areaList)
+        initAdapterSpinner(binding.spinnerYear,vintageList)
+
+    }
+
+    private fun initAdapterSpinner(spinner: Spinner, list: MutableList<String>){
+        val adapter =
+            ArrayAdapter(
+                requireContext(),
+                androidx.transition.R.layout.support_simple_spinner_dropdown_item,
+                list
+            )
+        spinner.adapter = adapter
+    }
+
 
     private fun initclicks() {
 
-        binding.buttonEditEntries.setOnClickListener{
+        binding.buttonEditEntries.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_managerFragment)
         }
     }
@@ -127,10 +200,10 @@ class EntriesFragment : Fragment() {
                             val newEntry = ChildData(
                                 description,
                                 category,
-                                quantity ,
+                                quantity,
                                 unity,
-                                price ,
-                                type ,
+                                price,
+                                type,
                                 total
                             )
 
@@ -150,5 +223,7 @@ class EntriesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
 
